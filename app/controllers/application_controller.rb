edd8032a5c585dc31447c34b67623d7a6800bd08
Matt Neal <findme@mattneal.com.au>
2018-01-05 13:04:39 +1000

    Added logged in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # turn @current_user into a helper so it can be used in views
-  helper_method :current_user
+  helper_method :current_user, :is_logged_in?
 
   # run this on every single page and action
   before_action :current_user
 
   def current_user
-    if session[:user_id].present?
+    if is_logged_in?
       @current_user = User.find(session[:user_id])
     else
       @current_user = nil
@@ -16,4 +16,7 @@ class ApplicationController < ActionController::Base
     @current_user
   end
 
+  def is_logged_in?
+    session[:user_id].present?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,36 @@
 class SessionsController < ApplicationController
+
+  def new
+    # we dont need any variables
+    # because we're not adding to the database
+    # just a session instead
+  end
+
+  def create
+    @username = form_params[:username]
+    @password = form_params[:password]
+
+    @user = User.find_by(username: @username).try(:authenticate, @password)
+
+    if @user.present?
+      session[:user_id] = @user.id
+      flash[:success] = "You are now logged in!"
+
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    reset_session
+
+    flash[:success] = "You are now logged out!"
+
+    redirect_to root_path
+  end
+
+  def form_params
+    params.require(:session).permit(:username,:password)
+  end
 end
