ece54d605d35f52bd7bb468edc83532f7ab9777e
Matt Neal <findme@mattneal.com.au>
2018-01-06 17:22:29 +1000

    Index page for items

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  # items
+  resources :items
+
+  # users cna sign up multiple times
   resources :users
 
+  # users can only make one session
   resource :session
-  
+
   get "about", to: "pages#about"
   root "pages#home"
 
