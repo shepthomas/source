1595816d4ae9d6db606eedf1a9491aef78df52b8
Matt Neal <findme@mattneal.com.au>
2018-01-07 20:30:26 +1000

    set up categories model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  # categories
+  resources :categories
+
   # items
   resources :items
 
